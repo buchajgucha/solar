69d11a7aad511fc9b6b863dcb2b58bdf47c846f8
buchajgucha <sportnashevse@yandex.ru>
2022-06-03 19:11:55 +0300

    final

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    loopFillGroupWithBlank: true,
+    navigation: {
+      nextEl: ".slider-arrowright",
+      prevEl: ".slider-arrowleft",
+    },
+  });
